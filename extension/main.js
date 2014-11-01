59615fa753290048e4c917b391ca27b523ad546e
Ilya Groshev <groshev.i.d@gmail.com>
2014-11-01 15:55:09 +0300

    appending select and button

--- extension/main.js
+++ extension/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
-    selectsource = "<select><option>none</option><option>pgp</option><option>priv</option></select>"
-   $("#im_user_holder").append(selectsource);
+    selectsource = "<select id='extcrypt-select' style='width: 50px;'><option value='no'>no</option><option value='pgp'>pgp</option><option value='ots'>ots</option></select>";
+    buttonsource = "<button id='extcrypt-button' style='width: 50px;'>Crypt</button>";
+    $("#im_user_holder").append(selectsource + buttonsource);
+    $("#extcrypt-button").click(function(){
+        if ($("#extcrypt-select").val() == 'pgp'){
+            $(".im_editable").text("pgp");
+        }
+
+        if ($("#extcrypt-select").val() == 'ots'){
+            $(".im_editable").text("ots");
+        }
+    });
 });
