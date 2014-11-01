f51e8dda00dd6ac6415956f9f8d3d4197f684b3a
Ilya Groshev <groshev.i.d@gmail.com>
2014-11-01 15:09:16 +0300

    openpgp lib added

--- extension/main.js
+++ extension/main.js
@@ -1,1 +1,4 @@
-alert("tst");
+$(document).ready(function(){
+    selectsource = "<select><option>none</option><option>pgp</option><option>priv</option></select>"
+   $("#im_user_holder").append(selectsource);
+});
