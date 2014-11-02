95c2807e487eac623c261189b932305ce9c08200
Ilya Groshev <groshev.i.d@gmail.com>
2014-11-02 18:47:46 +0300

    some changes

--- extension/main.js
+++ extension/main.js
@@ -1,31 +1,36 @@
 var parse = function(){
     $(".im_msg_text").each(function(index){
-        text =  CryptoGuy.Decrypt($(this).text());
+        text =  pgp.Decrypt($(this).text());
         if (text){
             $(this).text(text);
         }
     });
-}
+};
+
+var buttonsource = "<div class='button_blue im_send_cont fl_l' ><button id='extcrypt-button'>Crypt</button></div>";
+var cryptotype = "";
 $(document).ready(function(){
-    buttonsource = "<div class='button_blue im_send_cont fl_l' ><button id='extcrypt-button'>Crypt</button></div>"
     $("#im_send_wrap").prepend( buttonsource);
     $("#extcrypt-button").click(function(){
-        type = "";
         chrome.storage.sync.get({
             publicKey: '',
             privateKey: '',
             passphrase: '',
             type: 'no'
         }, function(items) {
-            type = items.type;
+            cryptotype = items.type;
         });
-        if (type == "pgp"){
-            text = CryptoGuy.Encrypt($("#im_editable").text());
+
+       /* if (cryptotype == "pgp"){
+
             if (text){
                 $("#im_editable").text(text);
             } else {
                 alert("Encrypting error!");
             }
+        }*/
+        if (cryptotype == "ots"){
+            console.log("ots");
         }
     });
     setTimeout(parse, 1000);
